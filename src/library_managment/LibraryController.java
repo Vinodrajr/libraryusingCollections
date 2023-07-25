0cc4c4145d26c38a8652094ba248b7889360b3f2
unknown <vinod.r@testyantra.com>
2023-07-25 10:26:34 +0530

    basic CRUD completed

--- src/library_managment/LibraryController.java
+++ src/library_managment/LibraryController.java
@@ -8,7 +8,7 @@ public class LibraryController {
 
 	Scanner scanner = new Scanner(System.in);
 	Library library = new Library();
-	List<Book> books = new ArrayList<Book>();
+	List<Book> list = new ArrayList<Book>();
 
 	public void createLibrary() {
 		System.out.println("Enter the Library name");
@@ -25,7 +25,7 @@ public class LibraryController {
 		library.setPhone(libphone);
 		library.setPincode(libpincode);
 
-		System.out.println(library);
+		System.out.println("Library Created");
 	}
 
 	public void addBook() {
@@ -46,9 +46,51 @@ public class LibraryController {
 		book.setTitle(title);
 		book.setAuthor(author);
 
-		books.add(book);
-		library.setBooks(books);
-		System.out.println(library);
+		list.add(book);
+		library.setBooks(list);
+		System.out.println("Book added sucessfully");
+
+	}
+
+	public void getAllBooks() {
+		for (Book book : list) {
+			System.out.println("The book name is  " + book.getName());
+			System.out.println("The book's author is " + book.getAuthor());
+			System.out.println("The book's price is " + book.getCost());
+			System.out.println("========================================");
+
+		}
+
+	}
+
+	public void deleteBook() {
+		int i = 1;
+		for (Book book : list) {
+			System.out.println(i + " Name: " + book.getName() + " Book price: " + book.getCost());
+			i++;
+		}
+		System.out.println("Enter the Id of the book to be deleted");
+		int choice = scanner.nextInt();
+		list.remove(choice - 1);
+		System.out.println("Book Deleted Sucessfully");
+
+	}
+
+	public void updateBook() {
+		int i = 1;
+		for (Book book : list) {
+			System.out.println(i + " Name: " + book.getName() + " Book price: " + book.getCost());
+			i++;
+		}
+		System.out.println("Enter the Id of the book to change the name");
+		int choice = scanner.nextInt();
+		Book book = list.get(choice - 1);
+		System.out.println("Enter the name to be Updated");
+		String bookname = scanner.next();
+		book.setName(bookname);
+		list.remove(choice - 1);
+		list.add(book);
+		System.out.println("Book is Updated");
 
 	}
 
--- src/library_managment/LibraryView.java
+++ src/library_managment/LibraryView.java
@@ -13,7 +13,36 @@ public class LibraryView {
 			int choice = sc.nextInt();
 			switch (choice) {
 			case 1: {
-				controller.createLibrary();
+				controller.createLibrary();// creating the Lib
+
+				boolean b1 = true;
+				while (b1) {
+					System.out.println("1. Add Book \n2.Get All books\n3.Delete Book\n4. Update Book");
+					int bookchoice = sc.nextInt();
+					switch (bookchoice) {
+					case 1: {
+						controller.addBook();
+					}
+						break;
+					case 2: {
+
+						controller.getAllBooks();
+					}
+						break;
+					case 3: {
+						controller.deleteBook();
+					}
+						break;
+					case 4: {
+						controller.updateBook();
+					}
+						break;
+					default: {
+						b1 = false;
+					}
+					}
+
+				}
 			}
 				break;
 			case 2: {
